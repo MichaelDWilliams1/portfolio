825c7efbd6d6d5f290a9c76bf36cf8461d6e9a44
MichaelDWilliams1 <102554995+MichaelDWilliams1@users.noreply.github.com>
2022-08-31 11:37:20 -0700

    Project section will now have working links, title, and description

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import {Link} from 'react-router-dom'
 import countryGIF from '../assets/countryProject.gif'
 import countryScreenShot from '../assets/countryScreen.png'
 import blackJackGif from '../assets/blackjackGif.gif'
@@ -37,28 +38,29 @@ function Projects(){
 
     return(
        <section className='projectContainer'>
+        <h2 className='projectContainerTitle'>Projects</h2>
         <div className='projectsHolder'>
         <div className='projects'>
             <div id='One' onMouseEnter={(e)=> gifToggleOn(e)} onMouseLeave={(e)=> gifToggleOff(e)}   className='projectsGrid'>
                 <img id='One' className='projectGif' src={isHovered.One ? countryGIF : countryScreenShot} />
             </div>
             <div className='projectsGrid'>
-                <p>This app allows a user to search any country in the world and find some basic information on that country. 
+                <p><h4 className='projectDescriptionTitle'>Where in the world?</h4>This app allows a user to search any country in the world and find some basic information on that country. 
                     The user can also toggle between light and dark mode.        
                 </p>
                 <div className='projectButtonsContainer'>
-                <button className='projectButton'>Code</button><button className='projectButton'>Website</button>
+                <motion.a whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} href='https://github.com/MichaelDWilliams1/rest-countries-api' className='projectButton'>Code</motion.a><motion.a whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} href='https://phenomenal-donut-0fdd1c.netlify.app/' className='projectButton'>Website</motion.a>
                 </div>
             
             </div>
         </div>
         <div  className='projects'>
             
-            <div className='projectsGrid '><p className='leftText'>This is my version of the classic casino game, Blackjack. You start the game with 500 dollars and you can either get rich
+            <div className='projectsGrid '><p className='leftText'><h4 className='projectDescriptionTitle'>Blackjack</h4>This is my version of the classic casino game, Blackjack. You start the game with 500 dollars and you can either get rich
                 or go broke trying. 
                 </p>
                 <div className='projectButtonsContainer'>
-                <button className='projectButton'>Code</button><button className='projectButton'>Website</button>
+                <motion.a whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} href='https://github.com/MichaelDWilliams1/Blackjack' className='projectButton'>Code</motion.a><motion.a whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} href='https://wonderful-blackwell-74b22b.netlify.app/' className='projectButton'>Website</motion.a>
                 </div></div>
             <div id='Two' onMouseEnter={(e)=> gifToggleOn(e)} onMouseLeave={(e)=> gifToggleOff(e)}  className='projectsGrid'>
                 <img id='Two' className='projectGif' src={isHovered.Two ? blackJackGif : blackJackScreen}  />
@@ -68,10 +70,10 @@ function Projects(){
             <div id='Three' onMouseEnter={(e)=> gifToggleOn(e)} onMouseLeave={(e)=> gifToggleOff(e)} className='projectsGrid'>
             <img id='Three' className='projectGif' src={isHovered.Three ? matchingGif : matchingScreen}  />
             </div>
-            <div className='projectsGrid'><p>This is a simple math matching game where the user has one minute to try and matching
+            <div className='projectsGrid'><p><h4 className='projectDescriptionTitle'>Match the math</h4>This is a simple math matching game where the user has one minute to try and matching
                 the simple match problem to the correct answer.</p>
                 <div className='projectButtonsContainer'>
-                <button className='projectButton'>Code</button><button className='projectButton'>Website</button>
+                <motion.a href='https://github.com/MichaelDWilliams1/Simple-Math-Matching-Game' whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} className='projectButton'>Code</motion.a><motion.a href='https://unruffled-pike-faa01a.netlify.app/' whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} className='projectButton'>Website</motion.a>
                 </div></div>
         </div>
         </div>
