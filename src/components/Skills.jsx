4d42501d49c4ec9f66b6d51d14ccce765c00b624
MichaelDWilliams1 <102554995+MichaelDWilliams1@users.noreply.github.com>
2022-09-02 14:50:17 -0700

    changed background of skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 const Skills = () => {
   return (
    <section className='skillsContainer'>
-    <h2 className='projectContainerTitle'>Skills</h2>
+    <h2 className='skillsTitle projectContainerTitle'>Skills</h2>
     <div  className='iconContainer'>
-        <div className='iconFlex'><i class="fa-brands fa-node"></i><p>NodeJS</p></div>
-        <div className='iconFlex'><i class="fa-brands fa-square-js"></i><p>JavaScript</p></div>
-        <div className='iconFlex'><i class="fa-brands fa-css3-alt"></i><p>CSS3</p></div>
-        <div className='iconFlex'><i class="fa-brands fa-html5"></i><p>HTML5</p></div>
-        <div className='iconFlex'><i class="fa-brands fa-react"></i><p>ReactJS</p></div>
+        <div className='iconFlex'><i class="fa-brands fa-node"></i><p className='iconText'>NodeJS</p></div>
+        <div className='iconFlex'><i class="fa-brands fa-square-js"></i><p className='iconText'>JavaScript</p></div>
+        <div className='iconFlex'><i class="fa-brands fa-css3-alt"></i><p className='iconText'>CSS3</p></div>
+        <div className='iconFlex'><i class="fa-brands fa-html5"></i><p className='iconText'>HTML5</p></div>
+        <div className='iconFlex'><i class="fa-brands fa-react"></i><p className='iconText'>ReactJS</p></div>
     </div>
 
    </section>
