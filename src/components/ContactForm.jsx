c8b342b975a45c50a7ec2e42ae2637b2f93e5374
MichaelDWilliams1 <102554995+MichaelDWilliams1@users.noreply.github.com>
2022-09-07 23:01:14 -0700

    fixed error with toastify notifications

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import emailjs from 'emailjs-com'
 
 const ContactForm = () => {
