189df41a8538dcd1dee5750f5755120ddcb12110
MichaelDWilliams1 <102554995+MichaelDWilliams1@users.noreply.github.com>
2022-09-07 22:57:14 -0700

    fixed some responsive issues. Added toastify for notifications when an email is sent

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,18 +1,48 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { ToastContainer, toast } from 'react-toastify';
 import emailjs from 'emailjs-com'
 
 const ContactForm = () => {
+const [formInfo, setFormInfo] = useState(
+  {
+    name: '',
+    email: '',
+    subject: '',
+    message: ''
+  }
+)
+
+const onChange = (event) => {
+  setFormInfo(prev => ({
+    ...prev,
+    [event.target.id]: event.target.value
+  }))
+  console.log(formInfo)
+}
 
 const sendEmail = (e) => {
 e.preventDefault()
 
+if(e.target[0].value === '' || e.target[1].value === '' || e.target[2].value === '' || e.target[3].value === '') 
+return toast.error('Please fill out all sections of the form', {
+  position: toast.POSITION.TOP_RIGHT
+})
+
     emailjs.sendForm('service_2a9fpxf', 'template_ltktbph', e.target, '93KL5tpAXDzsxtpnO')
     .then((result) => {
-        console.log(result.text);
+        toast.success('email sent!');
+        setFormInfo(  {
+          name: '',
+          email: '',
+          subject: '',
+          message: ''
+        })
     }, (error) => {
         console.log(error.text);
+        toast.error("there was an error")
     });
     e.target.reset()
+    
 }
    
 
@@ -21,15 +51,16 @@ e.preventDefault()
     <form  onSubmit={sendEmail}>
         <h6 className='emailTitle'>Send me a message</h6>
  
-    <input placeholder='name' type="text" name="name" />
+    <input placeholder='name' type="text" name="name" id='name' onChange={onChange} value={formInfo.name}/>
  
-    <input placeholder='email' type="email" name="email" />
+    <input placeholder='email' type="email" name="email" id='email'  onChange={onChange} value={formInfo.email}/>
 
-    <input placeholder='subject' type="text" name="subject" />
+    <input placeholder='subject' type="text" name="subject" id='subject'  onChange={onChange} value={formInfo.subject}/>
   
-    <textarea placeholder='message' name="message" />
+    <textarea placeholder='message' name="message" id='message'  onChange={onChange} value={formInfo.message}/>
     <input type="submit" value="Send Message" />
   </form>
+  <ToastContainer />
   </div>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -11,11 +11,11 @@ function Header(){
             <h2>Frontend Web Developer.</h2>
             <p>Navy Verteran 🇺🇸 turned Software Engineer 💻</p>
             <p>I have a passion for building web applications that</p>
-            <p>make the user experience a pleasant one</p>
-            <p>I love to learn, eat pizza 🍕, and drink entirely too much coffee ☕️</p>
+            <p>make the user experience a pleasant one.</p>
+            <p>I love to learn new things, read, play videos games, and drink entirely too much coffee ☕️</p>
         </div>
         <div className='buttonContainer'>
-            <motion.button whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }}>resume</motion.button>
+            <motion.button whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }}><a className='leftButton' href='https://docs.google.com/document/d/e/2PACX-1vSNjbf1qD5eUANo-1k1_spymUyv9X-nmZnV8pA9VTlyI1m2_VQLiIRW2EvjojDDiHMvHowhW3ncMMxC/pub'>resume</a></motion.button>
             <Link spy={true} smooth={true} to='projectSection'><motion.button whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} className='rightButton'>projects</motion.button></Link>
         </div>
         </>
