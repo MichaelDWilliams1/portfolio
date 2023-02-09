63e9948022e8f9ef5393dadc7abfc68290f996d2
MichaelDWilliams1 <102554995+MichaelDWilliams1@users.noreply.github.com>
2023-02-09 14:04:34 -0800

    portfolio complete overhaul. Initial commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React from 'react'
+import Header from './Header';
 
-import Navigation from './components/Navigation';
-import Header from './components/Header';
-import Projects from './components/Projects';
-import Skills from './components/Skills';
-import ContactForm from './components/ContactForm';
 
 function App() {
   return (
     <div className="App">
-   <Navigation />
- 
-   <Header />
-   <Skills />
-   <Projects />
-  <ContactForm />
+ <Header />
+
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='headerBG h-screen'>Header sds</div>
+  )
+}
+
+export default Header
