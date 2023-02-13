7bf6da8b5fa4270f1e351c5d1478cfca04cef734
MichaelDWilliams1 <102554995+MichaelDWilliams1@users.noreply.github.com>
2023-02-13 15:34:53 -0800

    skills grid built. resized for tablet and desktop.

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import reactSVG from '../assets/react.svg'
+import bootstrap from '../assets/bootstrap.svg'
+import css from '../assets/css.svg'
+import express from '../assets/express.svg'
+import html from '../assets/html.svg'
+import jest from '../assets/jest.svg'
+import mongo from '../assets/mongo.svg'
+import tailwind from '../assets/tailwind.svg'
 import js from '../assets/js.svg'
+
+
+
 const skills = () => {
   return (
-    <section className='h-1/2'>
-  fldjfadjkfjk
+    <section className='h-screen'>
+      <div className='h-full w-10/11 lg:w-3/5 m-auto'>
+        <ul className='grid grid-cols-3 grid-rows-3 h-full lg:text-3xl font-IBM mt-10 mb-10 border-2'>
+          <li className='flex flex-col items-center justify-around'><img className='h-3/4' src={js} /><p>Javascript</p></li>
+          <li className='flex flex-col items-center justify-around'><img className='h-3/4' src={reactSVG} /><p>React</p></li>
+          <li className='flex flex-col items-center justify-around'><img className='h-3/4' src={express} /><p>Javascript</p></li>
+          <li className='flex flex-col items-center justify-around'><img className='h-3/4' src={mongo} /><p>Javascript</p></li>
+          <li className='flex flex-col items-center justify-around'><img className='h-3/4' src={html} /><p>Javascript</p></li>
+          <li className='flex flex-col items-center justify-around'><img className='h-3/4' src={css} /><p>Javascript</p></li>
+          <li className='flex flex-col items-center justify-around'><img className='h-3/4' src={jest} /><p>Javascript</p></li>
+          <li className='flex flex-col items-center justify-around'><img className='h-3/4' src={tailwind} /><p>Javascript</p></li>
+          <li className='flex flex-col items-center justify-around'><img className='h-3/4' src={bootstrap} /><p>Javascript</p></li>
+        </ul>
+      </div>
+ 
     </section>
   )
 }
