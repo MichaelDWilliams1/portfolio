2e6994d6b069f373da8c81307b2cfeafda097a78
MichaelDWilliams1 <102554995+MichaelDWilliams1@users.noreply.github.com>
2023-02-18 11:24:00 -0800

    Skills section is done for now. Resized for any screen size

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -14,17 +14,18 @@ import js from '../assets/js.svg'
 const skills = () => {
   return (
     <section className='h-screen'>
-      <div className='h-full w-10/11 lg:w-3/5 m-auto'>
-        <ul className='grid grid-cols-3 grid-rows-3 h-full lg:text-3xl font-IBM mt-10 mb-10 border-2'>
-          <li className='flex flex-col items-center justify-around'><img className='h-3/4' src={js} /><p>Javascript</p></li>
-          <li className='flex flex-col items-center justify-around'><img className='h-3/4' src={reactSVG} /><p>React</p></li>
-          <li className='flex flex-col items-center justify-around'><img className='h-3/4' src={express} /><p>Javascript</p></li>
-          <li className='flex flex-col items-center justify-around'><img className='h-3/4' src={mongo} /><p>Javascript</p></li>
-          <li className='flex flex-col items-center justify-around'><img className='h-3/4' src={html} /><p>Javascript</p></li>
-          <li className='flex flex-col items-center justify-around'><img className='h-3/4' src={css} /><p>Javascript</p></li>
-          <li className='flex flex-col items-center justify-around'><img className='h-3/4' src={jest} /><p>Javascript</p></li>
-          <li className='flex flex-col items-center justify-around'><img className='h-3/4' src={tailwind} /><p>Javascript</p></li>
-          <li className='flex flex-col items-center justify-around'><img className='h-3/4' src={bootstrap} /><p>Javascript</p></li>
+      <div className='h-full w-10/11 lg:w-3/5 m-auto flex flex-col items-center justify-center font-IBM'>
+        <p className='text-5xl'>Skills</p>
+        <ul className='grid  grid-rows-3 w-full h-3/4 md:text-3xl lg:text-3xl mt-10 mb-10 grid-flow-col'>
+          <li className='flex flex-col items-center justify-around'><img className='h-1/2' src={js} /><p>Javascript</p></li>
+          <li className='flex flex-col items-center justify-around'><img className='h-1/2' src={reactSVG} /><p>React</p></li>
+          <li className='flex flex-col items-center justify-around'><img className='h-1/2' src={express} /><p>Express</p></li>
+          <li className='flex flex-col items-center justify-around'><img className='h-1/2' src={mongo} /><p>MongoDB</p></li>
+          <li className='flex flex-col items-center justify-around'><img className='h-1/2' src={html} /><p>HTML</p></li>
+          <li className='flex flex-col items-center justify-around'><img className='h-1/2' src={css} /><p>CSS</p></li>
+          <li className='flex flex-col items-center justify-around'><img className='h-1/2' src={jest} /><p>Jest</p></li>
+          <li className='flex flex-col items-center justify-around'><img className='h-1/2' src={tailwind} /><p>Tailwindcss</p></li>
+          <li className='flex flex-col items-center justify-around'><img className='h-1/2' src={bootstrap} /><p>Bootstrap</p></li>
         </ul>
       </div>
  
